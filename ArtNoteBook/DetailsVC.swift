2a2f76a4fdc15c24e0be1b3c665bd403c0718288
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-25 15:59:22 +0300

    fetch core data

--- ArtNoteBook/DetailsVC.swift
+++ ArtNoteBook/DetailsVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -45,6 +46,27 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     }
 
     @IBAction func saveButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        newPainting.setValue(nameTextField.text, forKey: "name")
+        newPainting.setValue(artistTextField.text, forKey: "artist")
+        if let year = Int(yearTextField.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newPainting.setValue(data, forKey: "image")
+        
+        do{
+            try context.save()
+            print("success")
+        }catch{
+                print("error")
+        }
     }
     
 }
