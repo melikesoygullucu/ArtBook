97290bfdb128286af3d024a215172e44fc6270b3
melikesoygullucu <melikesoygul@hotmail.com>
2024-06-26 10:53:22 +0300

    display previously added items

--- ArtNoteBook/DetailsVC.swift
+++ ArtNoteBook/DetailsVC.swift
@@ -15,10 +15,56 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
     @IBOutlet weak var artistTextField: UITextField!
     @IBOutlet weak var yearTextField: UITextField!
     
+    var chosenPainting = ""
+    var chosenPaintingID : UUID?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        if chosenPainting != "" {
+            // Core Data
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = chosenPaintingID?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameTextField.text = name
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistTextField.text = artist
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearTextField.text = String(year)
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                        }
+                        
+                    }
+                }
+            } catch {
+                print("error")
+            }
+            
+        } else {
+            nameTextField.text = ""
+            artistTextField.text = ""
+            yearTextField.text = ""
+        }
+
         //Recognizers
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(gestureRecognizer)
@@ -67,6 +113,10 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         }catch{
                 print("error")
         }
+        
+        // inform view controller for new datas
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newData"), object: nil)
+        self.navigationController?.popViewController(animated: true)
     }
     
 }
--- ArtNoteBook/ViewController.swift
+++ ArtNoteBook/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var tableView: UITableView!
     var nameArray = [String]()
     var idArray = [UUID]()
-
+    var selectedPainting = ""
+    var selectedPaintingID : UUID?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +28,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         getData()
     }
     
-    func getData() {
+    // if new data added and messages matched then reload the page
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    
+    @objc func getData() {
+        
+        // avoid duplicate data
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
@@ -35,14 +47,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         do{
             let results = try context.fetch(fetchRequest)
-            for result in results as! [NSManagedObject] {
-                if let name = result.value(forKey: "name") as? String{
-                    self.nameArray.append(name)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let name = result.value(forKey: "name") as? String{
+                        self.nameArray.append(name)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    self.tableView.reloadData()
                 }
-                if let id = result.value(forKey: "id") as? UUID {
-                    self.idArray.append(id)
-                }
-                self.tableView.reloadData()
             }
         }catch{
             print("error")
@@ -61,7 +75,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return nameArray.count
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedPainting = nameArray[indexPath.row]
+        selectedPaintingID = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.chosenPainting = selectedPainting
+            destinationVC.chosenPaintingID = selectedPaintingID
+        }
+    }
+    
     @objc func addButton() {
+        selectedPainting = ""
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 }
